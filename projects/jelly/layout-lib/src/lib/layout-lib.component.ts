88e97005e4b43d076143fd59fc1fdd09f646c20b
kantczak <kantczak@isolution.pl>
2020-03-06 21:15:44 +0100

    generate new project

---
+++ projects/jelly/layout-lib/src/lib/layout-lib.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jel-layout-lib',
+  template: `
+    <p>
+      layout-lib works!
+    </p>
+  `,
+  styles: []
+})
+export class LayoutLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/jelly/layout-lib/src/lib/layout-lib.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { LayoutLibComponent } from './layout-lib.component';
+
+
+
+@NgModule({
+  declarations: [LayoutLibComponent],
+  imports: [
+  ],
+  exports: [LayoutLibComponent]
+})
+export class LayoutLibModule { }
